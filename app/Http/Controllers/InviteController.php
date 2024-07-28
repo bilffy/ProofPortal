217c1c827f4dc8d2ac16a7752fbcd3818593e851
Joel C. Llano <oopcreator@gmail.com>
2024-07-29 06:41:29 +0800

    Add resource to invite multiple set of users

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -27,7 +27,7 @@ class InviteController extends Controller
     /**
      * Invite a single user.
      */
-    public function inviteSingleUser(string $id)
+    public function inviteSingleUser(int $id)
     {
         // Retrieve the user by ID
         /** @var User $user */
@@ -38,6 +38,37 @@ class InviteController extends Controller
         // Return a JSON response to indicate success
         return response()->json([
             'message' => 'Invite sent successfully.'
+        ]);
+    }
+
+    /**
+     * Invite multiple users.
+     * 
+     * Example Request:
+     * POST /invite
+     * {
+     *      "user_ids": [1, 2, 3, 4]
+     * }
+     */
+    public function inviteMultipleUsers(Request $request)
+    {
+        // Validate the request
+        $validated = $request->validate([
+            'user_ids' => 'required|array',
+            'user_ids.*' => 'exists:users,id',
+        ]);
+
+        $userIds = $validated['user_ids'];
+        $users = User::whereIn('id', $userIds)->get();
+
+        // Send invitations to each user
+        foreach ($users as $user) {
+            $this->userService->sendInvite($user->email, $user);
+        }
+
+        // Return a JSON response to indicate success
+        return response()->json([
+            'message' => 'Invites sent successfully to ' . count($users) . ' users.'
         ]);
     }
 }
