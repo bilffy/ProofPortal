1c99ba283cf649fd25ca3eca0632a06561fc9f4b
Arthur Baduyen <arthur.baduyen@chromedia.com>
2024-07-29 10:43:47 +0800

    button

--- resources/js/components/Button/BaseButton.vue
+++ resources/js/components/Button/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button type="button" 
-    class="px-3 py-2 rounded-md bg-none">
+    class="px-3 py-2 rounded-md bg-none text-sm cursor-pointer text-[#ffffff]">
     <slot />
   </button>
 </template>
