881d49c941ebd4adeb3df9e04605fe7b0861b1a9
Joel C. Llano <oopcreator@gmail.com>
2024-08-05 22:22:27 +0800

    Fix seeder, username should not be blank

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        
+        $email = 'test@example.com';
+        
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => $email,
+            'username' => $email,
+            'password' => bcrypt('password'),
         ]);
     }
 }
