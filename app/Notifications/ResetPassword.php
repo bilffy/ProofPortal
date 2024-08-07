a317f17d4d8176be1341fb0fc0709b22bcea74ab
Joel C. Llano <oopcreator@gmail.com>
2024-08-07 16:31:21 +0800

    Use custom Markdown email template

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -28,12 +28,13 @@ class ResetPassword extends Notification
     public function toMail($notifiable)
     {
         $expiration = config('auth.passwords.users.expire');
-        
+        $resetUrl = url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false));
+
         return (new MailMessage)
             ->subject('Reset Password Notification')
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
-            ->line("This password reset link will expire in {$expiration} minutes.")
-            ->line('If you did not request a password reset, no further action is required.');
+            ->markdown('emails.reset_password', [
+                'resetUrl' => $resetUrl,
+                'expiration' => $expiration,
+            ]);
     }
 }
