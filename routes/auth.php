c1dfc1dcef13be6797942230b0c137e4d580f4f0
Joel C. Llano <oopcreator@gmail.com>
2024-08-07 21:56:55 +0800

    Add new route and Controller to handle the Account setup process

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\AccountSetupController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
@@ -33,6 +34,10 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
+
+    Route::get('account-setup/{token}', [AccountSetupController::class, 'setup'])
+                ->name('account.setup');
+        
 });
 
 Route::middleware('auth')->group(function () {
