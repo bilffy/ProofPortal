9504c372036a0ea20aabcd4addbb92a86db0b92c
John Jewell Diño <johnjewell.dino@chromedia.com>
2024-08-10 01:58:55 +0800

    MSP-35: Add fontawesome libraries and component

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,6 +4,7 @@ import { createApp, h, type DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/src/js';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import FontAwesomeIcon from './fontawesome-icons';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,6 +14,7 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
+            .component('font-awesome-icon', FontAwesomeIcon)
             .use(plugin)
             .use(ZiggyVue)
             .mount(el)
