2e01695625308ce1e66ab3f8abbdceec9a5858db
John Jewell Diño <johnjewell.dino@chromedia.com>
2024-08-20 10:04:04 +0800

    MSP-42: Update link button

--- resources/views/components/button/link.blade.php
+++ resources/views/components/button/link.blade.php
@@ -1,3 +1,3 @@
- <x-button.baseButton type="type" class="px-3 py-2 bg-none text-neutral-600 hover:bg-[#f0eeec] transition-all hover:transition-all">
+ <x-button.base type="type" class="px-3 py-2 bg-none text-neutral-600 hover:bg-[#f0eeec] transition-all hover:transition-all">
     {{ $slot }}
-</x-button.baseButton>
+</x-button.base>
