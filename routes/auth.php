ecb26a56b08b36a548427745ccd09d8c40e0cd07
Joel C. Llano <oopcreator@gmail.com>
2024-08-28 20:09:05 +0800

    Convert reset password link to blade

--- routes/auth.php
+++ routes/auth.php
@@ -5,16 +5,15 @@ use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\AccountSetupController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\Auth\OtpController;
-use Illuminate\Support\Facades\Route;
 use App\Livewire\AccountSetup;
+use App\Livewire\Auth\ForgotPassword;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\OtpVerification;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -28,11 +27,13 @@ Route::middleware('guest')->group(function () {
 
     Route::get('login', Login::class)->name('login');
     
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    /*Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
+                ->name('password.email');*/
+
+    Route::get('/forgot-password', ForgotPassword::class)->name('password.request');
     
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
                 ->name('password.reset');
