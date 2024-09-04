3c3c4c9e1c0d5eb2797478a1b3c0ffa0de37cf41
Arthur Baduyen <arthur.baduyen@chromedia.com>
2024-09-04 19:51:47 +0800

    dropdownPanel

---
+++ resources/views/components/form/dropdownPanel.blade.php
@@ -0,0 +1,6 @@
+@props(['title'=>'modal title', 'id'=>'id'])
+
+<ul id="dropOptions" role="list" hidden class="absolute top-10 bg-white rounded shadow right-3 overflow-hidden">
+    {{ $slot}}
+</ul>
+ 
