6e245eb8f9fe8572fb973342a2cd5b664ffba411
Joel C. Llano <oopcreator@gmail.com>
2024-09-07 12:07:10 +0800

    Update Modal component to align with flowbite, ensure its props can be override

--- resources/views/components/modal/base.blade.php
+++ resources/views/components/modal/base.blade.php
@@ -1,26 +1,34 @@
+@props(
+    [
+        'id' => 'defaultModal',
+        'title' => 'Modal title',
+        'body' => null,
+        'footer' => null,
+    ]
+)
 <div 
-    id="default-modal" 
-    tabindex="-1" 
-    aria-hidden="true" 
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50">
+    id = "{{ $id }}"
+    tabindex="-1"
+    aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow">
-            
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
-            <x-modal.header title="Modal title"/>
-            
+            <x-modal.header title="{{ $title }}" id="{{ $id }}" />
             <!-- Modal body -->
-            <x-modal.body>
-                <p>
-                    Etiam non lectus posuere, congue ipsum eu, bibendum dui. In iaculis dapibus sem, quis tristique quam varius quis. Nunc tortor nisi, tincidunt placerat nisl ac, fringilla placerat lacus. Nam mattis nec lectus id tincidunt. Vestibulum egestas nunc a tincidunt eleifend. Duis sed ante venenatis, malesuada lacus vel, faucibus mi. Cras eu interdum urna. Cras vulputate erat eu lacus sagittis posuere. Vestibulum sed dui metus. Fusce et purus finibus, finibus sapien vel, suscipit ligula. Nam vel sagittis mauris. Donec ultricies tristique elit, nec convallis elit eleifend quis.
-                </p>
-                <p>
-                    Maecenas eget bibendum massa, ut porta sem. Nam quis mollis mi, sit amet dictum urna. Praesent dignissim ultricies felis ut varius. Suspendisse potenti. Morbi eu diam faucibus, ornare metus id, ullamcorper metus. Mauris eu lobortis neque. Proin posuere imperdiet lectus at ultrices. Aliquam erat volutpat. Pellentesque in urna et dolor venenatis rutrum in eget nisl. Maecenas ut ultrices justo. Curabitur id ornare eros. Praesent mattis orci gravida, bibendum turpis vel, placerat leo.
-                </p>
-            </x-modal.body>
+            @if($body)
+                @component($body)
+                @endcomponent
+            @endif
             <!-- Modal footer -->
-            <x-modal.footer/>
+            @if($footer)
+                @component($footer)
+                @endcomponent
+            @endif
         </div>
     </div>
+    {{ $slot }}
 </div>
+
