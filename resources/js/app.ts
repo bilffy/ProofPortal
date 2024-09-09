c7d69a384b7e2b79e8d05c304eada406b0c4c850
Joel C. Llano <oopcreator@gmail.com>
2024-09-09 12:27:54 +0800

    feat: integrate Bootstrap 5.3.3 and cleanup unused code

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,22 +1,5 @@
 import './bootstrap';
-import 'flowbite';
-// import '.exports';
-// import { createApp } from 'vue2';
-// import Test from './components/Test.vue';
-// import BladeAuthenticatedLayout from './Shared/BladeAuthenticatedLayout.vue';
-// import { LAYOUT_RENDER } from './helpers/constants.helper';
 
-// const app = createApp({});
-
-// app
-//     .component('test-component', Test)
-//     .component('auth-layout', BladeAuthenticatedLayout)
-//     .provide('layout_render', LAYOUT_RENDER.BLADE)
-//     .mount("#app_blade_vue");
-
-// TODO: Remove if not needed after blade conversions
-
-// import { getCurrentNav } from './helpers/utils.helper.ts';
 import.meta.glob([
     '../assets/**',
 ]);
