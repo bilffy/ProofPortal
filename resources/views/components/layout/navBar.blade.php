697a08eaf9a846c536d0568b22833d37d071cea3
Joel C. Llano <oopcreator@gmail.com>
2024-10-03 07:56:52 +0800

    Add role-based navigation visibility in navBar component

--- resources/views/components/layout/navBar.blade.php
+++ resources/views/components/layout/navBar.blade.php
@@ -7,13 +7,27 @@
         />
     </div>
     <x-layout.navItem id="tabHome" navIcon="home" href="{{ route('dashboard') }}">Home</x-layout.navItem>
-    <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PHOTOGRAPHY</span>
-    <x-layout.navItem id="tabPortraits" navIcon="user" href="{{ route('dashboard') }}">Portraits</x-layout.navItem>
-    <x-layout.navItem id="tabGroups" navIcon="users" href="{{ route('dashboard') }}">Group</x-layout.navItem>
-    <x-layout.navItem id="tabSpecialEvents" navIcon="graduation-cap" href="{{ route('dashboard') }}">Special Events</x-layout.navItem>
-    <x-layout.navItem id="tabPromoPhotos" navIcon="camera" href="{{ route('dashboard') }}">Promo Photos</x-layout.navItem>
-    <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PROOFING</span>
-    <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('proofing') }}">Proofing</x-layout.navItem>
+
+    @hasanyrole('Franchise|School Admin|Photo Coordinator|Teacher')
+        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PHOTOGRAPHY</span>
+        <x-layout.navItem id="tabPortraits" navIcon="user" href="{{ route('dashboard') }}">Portraits</x-layout.navItem>
+        <x-layout.navItem id="tabGroups" navIcon="users" href="{{ route('dashboard') }}">Groups</x-layout.navItem>
+        <x-layout.navItem id="tabSpecialEvents" navIcon="graduation-cap" href="{{ route('dashboard') }}">Special Events</x-layout.navItem>
+        <x-layout.navItem id="tabPromoPhotos" navIcon="camera" href="{{ route('dashboard') }}">Promo Photos</x-layout.navItem>
+    @endhasanyrole
+    
+    @hasanyrole('Franchise|Photo Coordinator|Teacher')
+        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PROOFING</span>
+        <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('proofing') }}">Proofing</x-layout.navItem>
+    @endhasanyrole
+
+    @hasanyrole('School Admin')
+        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">ORDERING</span>
+        <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">ID Cards</x-layout.navItem>
+        <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">Photos</x-layout.navItem>
+        <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">Yearbooks</x-layout.navItem>
+    @endhasanyrole
+    
     @if ($UserAbilitiesHelper->canUseAdminTools(auth()->user()->getRole()))
         <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">ADMIN TOOLS</span>
         <x-layout.navItem id="tabManageUsers" navIcon="user-plus" href="{{ route('users') }}">Manage Users</x-layout.navItem>
