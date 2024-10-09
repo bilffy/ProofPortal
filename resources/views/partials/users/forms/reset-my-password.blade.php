cbb1e4e356bf33af04e7e7a6338bcf4c81fec700
Joel C. Llano <oopcreator@gmail.com>
2024-10-09 15:30:47 +0800

    Add reset password form and update authentication layout
    - Modified `authenticated.blade.php` for layout adjustments
    - Updated `auth.php` routes to accommodate new password reset functionality
    - Added new reset password form under `resources/views/partials/users/forms/reset-my-password.blade.php`
    - Introduced Livewire components in `app/Livewire/Profile/` for profile management

---
+++ resources/views/partials/users/forms/reset-my-password.blade.php
@@ -0,0 +1,50 @@
+
+<div class="relative w-1/3">
+    
+        <div class="py-4 flex items-center justify-between">
+            <h3 class="text-2xl">Update Password</h3>
+            <div></div>
+        </div>
+        <div class="relative overflow-x-auto mb-2 gap-4">
+            <form wire:submit.prevent="submit" x-data="{ password: @entangle('password'), password_confirmation: @entangle('password_confirmation') }">
+                
+                <div class="flex flex-col mb-4">
+                    <input class="border rounded-md p-2 border-neutral" type="password" wire:model="password" required placeholder="Password" autocomplete="new-password" />
+                    @error('password') <span class="error">{{ $message }}</span> @enderror
+                </div>
+
+                <div class="flex flex-col mb-4">
+                    <input class="border rounded-md p-2 border-neutral" type="password" wire:model="password_confirmation" required placeholder="Repeat Password" autocomplete="new-password" />
+                    @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
+                </div>
+
+                <div class="ml-4 mb-4">
+                    <ul class="list-disc">
+                        <li :class="{ 'text-success font-semibold': password.length >= 12, 'text-gray-500': password.length < 12 }">At least 12 characters</li>
+                        <li :class="{ 'text-success font-semibold': /[A-Z]/.test(password), 'text-gray-500': !/[A-Z]/.test(password) }">Include at least 1 uppercase letter</li>
+                        <li :class="{ 'text-success font-semibold': /[a-z]/.test(password), 'text-gray-500': !/[a-z]/.test(password) }">At least 1 lowercase letter</li>
+                        <li :class="{ 'text-success font-semibold': /[0-9]/.test(password), 'text-gray-500': !/[0-9]/.test(password) }">At least 1 number</li>
+                        <li :class="{ 'text-success font-semibold': password === password_confirmation, 'text-gray-500': password !== password_confirmation }">Passwords must match each other</li>
+                    </ul>
+                </div>
+
+                <div class="flex w-full items-center justify-between">
+                    <button
+                            class="rounded-md text-sm cursor-pointer bg-none text-[#ffffff] flex flex-row gap-1 px-3 py-2 bg-primary text-sm hover:bg-primary-hover transition-all hover:transition-all"
+                            type="submit"
+                            :disabled="!(password.length >= 12 && /[A-Z]/.test(password) && /[a-z]/.test(password) && /[0-9]/.test(password) && password === password_confirmation)">
+                        <span wire:loading.remove>Update</span>
+                        <span wire:loading><x-spinner.button label="Update" /></span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    
+</div>
+
+@push('scripts')
+    <script type="module">
+       
+    </script>
+
+@endpush
