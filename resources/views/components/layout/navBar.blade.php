acd03408d1fff37b58d661293d65d8adca3855b3
Arthur Baduyen <arthur.baduyen@chromedia.com>
2024-10-10 14:17:45 +0800

    rebase

--- resources/views/components/layout/navBar.blade.php
+++ resources/views/components/layout/navBar.blade.php
@@ -1,15 +1,29 @@
-<div id="navbarWrapper" class="flex flex-col w-[190px] min-w-[190px] mt-2">
-    <div class="p-4 flex justify-center">
+@props(['collapsed'=>true])
+
+<div class="flex flex-col mt-2 mr-2">
+    <div class="py-4 px-2 flex justify-center">
         <img 
         src="{{ Vite::asset('resources/assets/images/MSP-Logo.svg') }}" 
         alt=""
         width=125px
+        class="hideOnCollapse"
         />
+        <img 
+        src="{{ Vite::asset('resources/assets/images/MSP-logo-sm.svg') }}" 
+        alt=""
+        width=47px
+        class="showOnCollapse"
+        />
+    </div>
+    <div class="relative h-[40px] flex justify-end">
+        <span id="btnToggleNavBar" class="absolute bg-neutral p-2 hover:bg-primary-hover hover:cursor-pointer hover:transition -right-6 w-[32px] h-[32px] flex justify-center items-center rounded-full">
+            <x-icon id="btnCollapse" icon="arrow-left" style="color: #ffffff"/>
+        </span>
     </div>
     <x-layout.navItem id="tabHome" navIcon="home" href="{{ route('dashboard') }}">Home</x-layout.navItem>
 
     @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_PHOTOGRAPHY))
-        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PHOTOGRAPHY</span>
+        <span class="hideOnCollapse text-sm text-neutral-600 ml-4 font-bold mt-4">PHOTOGRAPHY</span>
         <x-layout.navItem id="tabPortraits" navIcon="user" href="{{ route('dashboard') }}">Portraits</x-layout.navItem>
         <x-layout.navItem id="tabGroups" navIcon="users" href="{{ route('dashboard') }}">Groups</x-layout.navItem>
         <x-layout.navItem id="tabSpecialEvents" navIcon="graduation-cap" href="{{ route('dashboard') }}">Special Events</x-layout.navItem>
@@ -17,33 +31,58 @@
     @endcan
     
     @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_PROOFING))
-        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">PROOFING</span>
+        <span class="hideOnCollapse text-sm text-neutral-600 ml-4 font-bold mt-4">PROOFING</span>
         <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('proofing') }}">Proofing</x-layout.navItem>
     @endcan
 
     @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_ORDERING))
-        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">ORDERING</span>
+        <span class="hideOnCollapse text-sm text-neutral-600 ml-4 font-bold mt-4">ORDERING</span>
         <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">ID Cards</x-layout.navItem>
         <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">Photos</x-layout.navItem>
         <x-layout.navItem id="tabProofing" navIcon="th" href="{{ route('dashboard') }}">Yearbooks</x-layout.navItem>
     @endcan
     
     @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_ADMIN_TOOLS))
-        <span class="text-sm text-neutral-600 ml-4 font-bold mt-4">ADMIN TOOLS</span>
+        <span class="hideOnCollapse text-sm text-neutral-600 ml-4 font-bold mt-4">ADMIN TOOLS</span>
         <x-layout.navItem id="tabManageUsers" navIcon="user-plus" href="{{ route('users') }}">Manage Users</x-layout.navItem>
         <x-layout.navItem id="tabReports" navIcon="list-ul" href="{{ route('dashboard') }}">Reports</x-layout.navItem>
     @endcan
-    <span class="text-sm ml-4 font-bold mt-4 text-alert">TESTING PAGE</span>
+    <span class="hideOnCollapse text-sm ml-4 font-bold mt-4 text-alert">TESTING PAGE</span>
     <x-layout.navItem id="tabSchoolHome" navIcon="home" href="{{ route('test2') }}">School Home</x-layout.navItem>
 </div>
 
 @push('scripts')
 <script type="module">
+    let navCollapsed = false;
     import { NAV_TABS } from "{{ Vite::asset('resources/js/helpers/constants.helper.ts') }}"
     import { getCurrentNav, getNavTabId } from "{{ Vite::asset('resources/js/helpers/utils.helper.ts') }}"
     window.addEventListener("load", function () {
         const targetElement = `#${getNavTabId(getCurrentNav())}`;
+        
         $(targetElement).addClass('bg-primary text-white rounded-e-md');
+        
+
+        //TODO temporary implmentation. waiting for State Management
+        $('img.showOnCollapse').toggle()
+        $('#btnToggleNavBar').on("click", function() {
+            if (navCollapsed) {
+                // Toggle icon
+                $('#btnCollapse ').toggleClass("fa-arrow-left fa-arrow-right")
+                $('img.hideOnCollapse').animate({width: 'toggle'})
+                $('img.showOnCollapse').animate({width: 'toggle'})
+                
+                $('span.hideOnCollapse').animate({width: 'toggle'})
+            } else {
+                // Toggle icon
+                $('#btnCollapse ').toggleClass("fa-arrow-left fa-arrow-right")
+                $('img.hideOnCollapse').animate({width: 'toggle'})
+                $('img.showOnCollapse').animate({width: 'toggle'})
+
+                $('span.hideOnCollapse').animate({width: 'toggle'})
+            }
+            navCollapsed =!navCollapsed
+        });
+     
     }, false);
 </script>
 
