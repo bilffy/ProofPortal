cf17100900e1c3b3834c9f08fe589d0e4ce42e0b
Joel C. Llano <oopcreator@gmail.com>
2024-11-04 09:52:13 +0800

    Fix styling and UI improvements

--- resources/views/layouts/authenticated.blade.php
+++ resources/views/layouts/authenticated.blade.php
@@ -7,46 +7,54 @@
             <header class="w-full flex justify-between pl-4 pr-4 mr-2 py-2 min-h-[68px] ">
                 <div class="flex items-center">
                     @php if ($SchoolContextHelper->isSchoolContext()) { @endphp
-                    <x-icon class="px-2" icon="arrow-left" />
-                    <a href="{{ route('dashboard') }}">Back</a>
-                    <x-icon class="px-2" icon="chevron-right fa-xs text-neutral-400" />
-                    <button
-                            id="userBtn"
-                            type="button"
-                            data-dropdown-toggle="BreadcrumbSelectSchool"
-                            class="inline-flex items-center px-3 py-2 border-transparent text-sm leading-4 font-medium rounded-md hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 text-gray-800"
-                    >
-                        {{ $SchoolContextHelper->getCurrentSchoolContext()->name }}
-                        <x-icon  class="px-2" icon="caret-up" hidden />
-                        <x-icon  class="px-2" icon="caret-down" />
-                    </button>
-                    <!-- Dropdown menu -->
-                    <div id="BreadcrumbSelectSchool" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
-                        <div class="p-3">
-                            <label for="input-group-search" class="sr-only">Search</label>
-                            <div class="relative">
-                                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                    </svg>
-                                </div>
-                                <input type="text" id="input-group-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search user">
-                            </div>
-                        </div>
-                        <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">
-                            @foreach ($SchoolContextHelper->getSchoolsByFranchise($user->resource->getFranchise()) as $school)
-                                <li>
-                                    <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <label for="checkbox-item-11" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
-                                            <a href="{{ route('school.view', ['id' => $school['id']]) }}">
-                                                {{ $school['name'] }}
-                                            </a>
-                                        </label>
+                        <span class="px-1">
+                            <a href="{{ route('dashboard') }}" class="inline-flex items-center px-3 py-2 border-transparent text-sm leading-4 font-medium rounded-md hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 text-gray-800">
+                                <x-icon class="mr-2" icon="arrow-left" />
+                                Back
+                            </a>
+                        </span>    
+                        <x-icon class="px-2" icon="chevron-right fa-xs text-neutral-400" />
+                        <button
+                                id="userBtn"
+                                type="button"
+                                data-dropdown-toggle="BreadcrumbSelectSchool"
+                                class="inline-flex items-center px-3 py-2 border-transparent text-sm leading-4 font-medium rounded-md hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 text-gray-800"
+                        >
+                            {{ $SchoolContextHelper->getCurrentSchoolContext()->name }}
+                            <x-icon  class="px-2" icon="caret-up" hidden />
+                            <x-icon  class="px-2" icon="caret-down" />
+                        </button>
+                        <!-- Dropdown menu -->
+                        <div id="BreadcrumbSelectSchool" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
+                            <div class="p-3">
+                                <label for="input-group-search" class="sr-only">Search</label>
+                                <div class="relative">
+                                    <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                        </svg>
                                     </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                                    <input type="text" id="input-group-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search user">
+                                </div>
+                            </div>
+                            <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">
+                                @foreach ($SchoolContextHelper->getSchoolsByFranchise($user->resource->getFranchise()) as $school)
+                                    <li>
+                                        <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                            <label for="checkbox-item-11" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
+                                                <a href="{{ route('school.view', ['id' => $school['id']]) }}">
+                                                    {{ $school['name'] }} 
+                                                </a>
+                                            </label>
+
+                                            @php if ($SchoolContextHelper->getCurrentSchoolContext()->id == $school['id']) { @endphp
+                                                <i class="fas fa-check"></i>
+                                            @php } @endphp
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
 
                     @php } @endphp
                     {{--<x-form.dropdownPanel id="BreadcrumbSelectSchool">
