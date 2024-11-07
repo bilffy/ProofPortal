cbb604485715527f75a41a9e42485082a4103f04
Arthur Baduyen <arthur.baduyen@chromedia.com>
2024-11-07 13:19:44 +0800

    Changed Manage User icon

--- resources/views/components/layout/navBar.blade.php
+++ resources/views/components/layout/navBar.blade.php
@@ -55,7 +55,7 @@
     
     @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_ADMIN_TOOLS))
         {{-- <span class="hideOnCollapse {{ $visibility }} whitespace-nowrap text-sm text-neutral-600 ml-4 font-bold mt-4">ADMIN TOOLS</span> --}}
-        <x-layout.navItem visibility="{{ $visibility }}" id="tabManageUsers" navIcon="user-plus" href="{{ route('users') }}">Manage Users</x-layout.navItem>
+        <x-layout.navItem visibility="{{ $visibility }}" id="tabManageUsers" navIcon="user" href="{{ route('users') }}">Manage Users</x-layout.navItem>
         @can ($PermissionHelper->getAccessToPage($PermissionHelper::SUB_REPORTS))
             <x-layout.navItem visibility="{{ $visibility }}" id="tabReports" navIcon="list-ul" href="{{ route('dashboard') }}">Reports</x-layout.navItem>
         @endcan
@@ -87,7 +87,7 @@
             $('#btnCollapse').toggleClass('fa-chevron-left fa-chevron-right');
             $('#btnCollapseExpand').toggleClass('pr-0 pr-5')
             $('div.hideOnCollapse').toggleClass('textCollapsed textExpanded')
-            
+
             // AJAX call to save collapsed state
             $.ajax({
                 url: '{{ route("navbar.toggleCollapse") }}',
