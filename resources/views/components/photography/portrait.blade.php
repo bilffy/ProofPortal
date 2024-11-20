18e40a35b609dac1aeef5611f9cfdd84d0564701
Arthur Baduyen <arthur.baduyen@chromedia.com>
2024-11-20 12:21:27 +0800

    Portrait component

--- resources/views/components/photography/portrait.blade.php
+++ resources/views/components/photography/portrait.blade.php
@@ -4,11 +4,14 @@
     'active' => false
 ])
 
-<div class="rounded-md w-[186] px-2 pt-2 flex flex-col align-middle justify-center {{ $active ? "bg-primary-100":"bg-transparent" }}">
+<div class="rounded-md w-[186] px-2 pt-2 flex flex-col align-middle justify-center "> {{-- {{ $active ? "bg-primary-100":"bg-transparent" }} --}}
     <div class="relative">
         <div class="absolute flex w-full justify-end pr-2 pt-2">
-            <div class="w-[24px] h-[24px] align-middle justify-center p-1 bg-white flex rounded-full group hover:cursor-pointer transition-all {{ $active ? 'hover:bg-primary':'hover:bg-primary-100'}}">
-                <x-icon icon="{{ $active ? 'check text-primary group-hover:text-white':'check text-white'}}"/>
+            <div class="group hover:cursor-pointer transition-all 
+                        w-[24px] h-[24px] p-1 pt-[3px] border-white border-2
+                        flex align-middle justify-center rounded-full 
+                        {{ $active ? 'bg-white hover:bg-primary-100':'hover:bg-primary-100'}}">
+                <x-icon icon="{{ $active ? 'check text-primary group-hover:text-white':''}}"/>
             </div>
         </div>
         <img 
@@ -17,17 +20,9 @@
         width=125px
         class="w-full max-w-none rounded"
     />
-    <div class="absolute flex w-full justify-end pr-2 pt-2 bottom-2">
-        <button>
-            <x-icon icon="download text-white fa-xl"/>
-        </button>
-    </div>
     </div>
     
-    <div class="flex justify-between p- py-2">
+    <div class="flex justify-between py-2 text-sm">
         <span>{{$name}}</span>
-        {{-- <button>
-            <x-icon icon="download"/>
-        </button> --}}
     </div>
 </div>
