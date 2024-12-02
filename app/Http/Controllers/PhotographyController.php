12ae60721a84e6cba213ea21eb183031ea8a1a58
John Jewell Diño <johnjewell.dino@chromedia.com>
2024-12-02 17:54:13 +0800

    MSP-123: Add templates and navigation for photography pages, Add navigation validations to specific roles for photography routes, Add controllers and middlewares, Modify handling of navigation to include change in url per sub route

--- app/Http/Controllers/PhotographyController.php
+++ app/Http/Controllers/PhotographyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\SchoolContextHelper;
 use Auth;
 use App\Http\Resources\UserResource;
 
@@ -9,6 +10,31 @@ class PhotographyController extends Controller
 {
     public function index()
     {
-        return view('photography', ['user' => new UserResource(Auth::user())]);
+        $user = Auth::user();
+        if ($user->isFranchiseLevel() && SchoolContextHelper::isSchoolContext()) {
+            return redirect()->route('photography.configure');
+        } else {
+            return redirect()->route('photography.portraits');
+        }
+    }
+
+    public function configure()
+    {
+        return view('photography', ['user' => new UserResource(Auth::user()), 'currentTab' => 'configure']);
+    }
+
+    public function showPortraits()
+    {
+        return view('photography', ['user' => new UserResource(Auth::user()), 'currentTab' => 'portraits']);
+    }
+
+    public function showGroups()
+    {
+        return view('photography', ['user' => new UserResource(Auth::user()), 'currentTab' => 'groups']);
+    }
+
+    public function showOthers()
+    {
+        return view('photography', ['user' => new UserResource(Auth::user()), 'currentTab' => 'others']);
     }
 }
