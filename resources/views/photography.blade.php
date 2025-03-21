5aad919b653c59b67886875ca5a56f96597602f4
Joel C. Llano <joel.llano@tabcorp.com.au>
2025-03-21 17:46:28 +0800

    Pass the nonce on the Header request

--- resources/views/photography.blade.php
+++ resources/views/photography.blade.php
@@ -331,14 +331,14 @@
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'MSP-Nonce': $(`#nonce`).val()
                 },
                 body: JSON.stringify(
                     { 
                         images: selectedImages,
                         category: selectedImages.length === 1 ? INDIVIDUAL_CATEGORY : BULK_CATEGORY,
                         filters: filters,
-                        nonce: $(`#nonce`).val()
                     }
                 )
             });
