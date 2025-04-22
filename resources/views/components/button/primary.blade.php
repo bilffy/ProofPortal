8a89cfd43362b71d7d6094af547e0296f6309f40
John Jewell Diño <johnjewell.dino@chromedia.com>
2025-04-22 22:08:59 +0800

    MSP-187: Adjust button styling

--- resources/views/components/button/primary.blade.php
+++ resources/views/components/button/primary.blade.php
@@ -1,7 +1,7 @@
 <x-button.base
 {{ $attributes->merge([
     'flavor'=>'primary',
-    'class'=>'px-3 py-2 transition-all hover:transition-all disabled:opacity-50 disabled:cursor-not-allowed',
+    'class'=>'px-3 py-2 transition-all hover:transition-all disabled:opacity-50 disabled:cursor-not-allowed hover:bg-primary-hover hover:border-primary-hover text-white',
 ]) }}
   >
     {{ $slot }}
