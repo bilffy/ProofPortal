789c356515e2ddcae66176e1a33ead19b3c986cf
Joel C. Llano <joel.llano@tabcorp.com.au>
2025-04-23 05:59:06 +0800

    Ensure iframe height of ordering responsive

--- resources/views/livewire/order.blade.php
+++ resources/views/livewire/order.blade.php
@@ -1,6 +1,6 @@
 <div class="relative overflow-x-auto">
     <div class="container3 p-4">
-        <iframe aria-label='MSP Photography Orders' frameborder="0" style="height:500px;width:99%;border:none;" 
+        <iframe aria-label='MSP Photography Orders' frameborder="0" style="width: 100%; height: 80vh; border: none;" 
                 src='https://forms.zohopublic.com.au/msp/form/IDCardOrderingviaPortal/formperma/znjNcllPz1C3h9cEX-B_jvCfwx-8z-7Vk94-K7_y-Ik'>
         </iframe>
     </div>
