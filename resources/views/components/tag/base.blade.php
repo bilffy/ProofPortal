4dd7f701262515825114bac5e0bd4a7bca20b1fd
John Jewell Diño <johnjewell.dino@chromedia.com>
2025-06-19 22:03:37 +0800

    MSP-243: Update base tag component

--- resources/views/components/tag/base.blade.php
+++ resources/views/components/tag/base.blade.php
@@ -1,15 +1,8 @@
-@props(['hollow' => false, 'flavor' => 'primary'])
-
-@php
-    $colorBorder = "border-{$flavor}";
-    $flavorBg = "bg-{$flavor}";
-    $colorBackground = $hollow ? "bg-none" : $flavorBg;
-    $textColor = $hollow ? "text-{$flavor}" : "text-white";
-@endphp
+@props(['hollow' => false])
 
 <span
     {{ $attributes->merge([
-        'class' => "rounded-md text-sm font-semi p-1 h-fit w-fit border-1 border-solid $textColor $colorBorder $colorBackground"
+        'class' => "bg-white border-1 border-solid rounded-md text-sm font-semi p-1 h-fit w-fit"
     ]) }}
 >
     {{ $slot }}
