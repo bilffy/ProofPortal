a0003673b02d7ec9a79fd518b880688d5b276a01
bilffy <bilffy.jose@msp.com.au>
2025-09-09 13:20:54 +1000

    changed code

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,8 +1,5 @@
 import './bootstrap';
-import 'flowbite'; // Flowbite JS components (Modal, etc.)
-import 'select2/dist/js/select2.full.min.js'; // Select2 ESM-compatible
-import 'select2/dist/css/select2.min.css'; // CSS for Select2
-
+import 'flowbite';
 import.meta.glob([
     '../assets/**',
 ]);
