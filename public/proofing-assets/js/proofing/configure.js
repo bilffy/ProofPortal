f039c6a8d01ddae48fba4668d6545cf69c6b4edf
bilffy <bilffy.jose@msp.com.au>
2025-09-10 11:35:58 +1000

    testing image upload

--- public/proofing-assets/js/proofing/configure.js
+++ public/proofing-assets/js/proofing/configure.js
@@ -267,7 +267,7 @@
                     }
                 };
         
-                Upload.prototype.successHandling = function (data) {
+                Upload.prototype.successHandling = function (data) {console.log(data);
                     if (data.error) {
                         $("#" + this.folder_key + "-bar").addClass('d-none');
                         $("#" + this.folder_key + "-error").removeClass('d-none').text(data.error.data.message);
