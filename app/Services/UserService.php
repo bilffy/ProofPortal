41c1334cbafffa1d4af41e19cdc4a02f71b181f7
Joel C. Llano <joel.llano@tabcorp.com.au>
2025-10-02 07:21:30 +0800

    Enforce user-school validation for photo grid

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Helpers\ActivityLogHelper;
 use App\Helpers\Constants\LogConstants;
 use App\Models\User;
+use App\Models\School;
 use App\Jobs\SendUserInviteJob;
 use App\Jobs\SendOTPJob;
 use App\Models\UserOtp;
@@ -44,5 +45,28 @@ class UserService
         return UserOtp::where('user_id', $user->id)
             ->latest('created_at')
             ->first();
+    }
+
+    /**
+     * Check if the user can access images of the given school.
+     * @param User $user
+     * @param School $school
+     * @return bool
+     */
+    public static function isCanAccessImage(User $user, School $school): bool
+    {
+        if ($user->isFranchiseLevel() && $school) {
+            // Make sure user belongs to the same school's franchise.
+            if (!$school->franchises()->where('franchise_id', $user->getFranchise()->id)->exists()){
+                return false;
+            }
+        } else {
+            // Make sure user has the same school being access with
+            if ($user->getSchool()->id !== $school->id) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
