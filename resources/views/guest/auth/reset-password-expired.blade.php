d2864a72a5b4909303b59f374bc3b97c66cc3440
bilffy <bilffy.jose@msp.com.au>
2026-04-07 15:46:39 +1000

    add login link to reset link expired

--- resources/views/guest/auth/reset-password-expired.blade.php
+++ resources/views/guest/auth/reset-password-expired.blade.php
@@ -3,8 +3,8 @@
     <p class="text-gray-600 mb-6">
         The link is invalid or has expired. Please request a new password reset link. 
         <div class="flex w-full items-center justify-between mt-4">
-            <a href="{{ route('login') }}" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Back to login
+            <a href="{{ route('password.request') }}" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Reset Password
             </a>
         </div>   
     </p>
