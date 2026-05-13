eb221f0dfd30dd4b7a2cfb88c1da513fe7356bd6
bilffy <bilffy.jose@msp.com.au>
2026-05-13 12:51:28 +1000

    added scheduler for sync images of proofing job

--- app/Jobs/SyncImagesToProd02.php
+++ app/Jobs/SyncImagesToProd02.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
-class SyncImagesToProd02 implements ShouldQueue
+class SyncImagesToProd02 implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -17,6 +18,14 @@ class SyncImagesToProd02 implements ShouldQueue
 
     // Give the job plenty of time to run (5 minutes)
     public $timeout = 300;
+
+    /**
+     * The unique ID of the job.
+     */
+    public function uniqueId(): string
+    {
+        return $this->jobKey;
+    }
 
     public function __construct($jobKey = null)
     {
--- routes/console.php
+++ routes/console.php
@@ -1,18 +1,37 @@
 <?php
 
-use App\Console\Commands\CleanUploadedImagesCommand;
+// use App\Console\Commands\CleanUploadedImagesCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use App\Models\Job;
+use App\Jobs\SyncImagesToProd02;
+use Illuminate\Support\Carbon;
 
-$logsPath = storage_path('logs/clean_uploaded_images.log');
+// $logsPath = storage_path('logs/clean_uploaded_images.log');
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(CleanUploadedImagesCommand::class)
-    ->timezone(env('APP_TIMEZONE', 'UTC'))
-    ->at('1:00')
-    ->runInBackground()
-    ->appendOutputTo($logsPath);
+// Schedule::command(CleanUploadedImagesCommand::class)
+//     ->timezone(env('APP_TIMEZONE', 'UTC'))
+//     ->at('1:00')
+//     ->runInBackground()
+//     ->appendOutputTo($logsPath);
+
+Schedule::call(function () {
+    $jobs = Job::where('proof_start', '>', Carbon::today())
+        ->whereHas('images', function ($query) {
+            $query->where('exportStatus', 0)
+                  ->where('keyorigin', 'Subject');
+        })
+        ->get();
+
+    foreach ($jobs as $job) {
+        SyncImagesToProd02::dispatch($job->ts_jobkey);
+    }
+})
+->everyFiveMinutes()
+->runInBackground()
+->withoutOverlapping();
