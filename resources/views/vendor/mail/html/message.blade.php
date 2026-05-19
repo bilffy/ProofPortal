aad7f98f6b0e88e26e528d73044fb3c07032ccbc
bilffy <bilffy.jose@msp.com.au>
2026-05-19 15:02:06 +1000

    font in email testing

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,46 +1,34 @@
 <x-mail::layout>
-    {{-- Header --}}
-    {{-- REMOVE HEADER
-    <x-slot:header>
-        <x-mail::header :url="config('app.url')">
-            {{ config('app.name') }}
-        </x-mail::header>
-    </x-slot:header>
-    --}}
-
-        {{-- Body --}}
-        {{ $slot }}
-
-        {{-- Subcopy --}}
-        @isset($subcopy)
-            <x-slot:subcopy>
-                <x-mail::subcopy>
-                    {{ $subcopy }}
-                </x-mail::subcopy>
-            </x-slot:subcopy>
-        @endisset
-
-        {{-- Footer --}}
-    <x-slot:footer>
-        <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="750" style="width: 750px; margin: 0 auto;">
-            <tr>
-                <td style="padding: 0; text-align: center;">
-                    <x-mail::footer>
-                        <!-- <p style="font-size: 12px; color: #666666; line-height: 1.3;">
-							{{$franchise->getBusinessName()}}
-							<br/>
-							@if (null == $franchise->state)
-								{{$franchise->address}}
-							@else
-								{{$franchise->address}}, {{$franchise->state}} {{$franchise->postcode}}
-							@endif
-							<br/><br/> -->
-						<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 12px; color: #666666; line-height: 1.3;">
-							Copyright ⓒ 2026 MSP Photography. All rights reserved.
-						</p>
-                    </x-mail::footer>
-                </td>
-            </tr>
-        </table>
-    </x-slot:footer>
+{{-- Header --}}
+{{-- REMOVE HEADER
+<x-slot:header>
+<x-mail::header :url="config('app.url')">
+{{ config('app.name') }}
+</x-mail::header>
+</x-slot:header>
+--}}
+{{-- Body --}}
+{{ $slot }}
+{{-- Subcopy --}}
+@isset($subcopy)
+<x-slot:subcopy>
+<x-mail::subcopy>
+{{ $subcopy }}
+</x-mail::subcopy>
+</x-slot:subcopy>
+@endisset
+{{-- Footer --}}
+<x-slot:footer>
+<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="750" style="width: 750px; margin: 0 auto;">
+<tr>
+<td style="padding: 0; text-align: center;">
+<x-mail::footer>
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 12px; color: #666666; line-height: 1.3;">
+Copyright ⓒ 2026 MSP Photography. All rights reserved.
+</p>
+</x-mail::footer>
+</td>
+</tr>
+</table>
+</x-slot:footer>
 </x-mail::layout>
