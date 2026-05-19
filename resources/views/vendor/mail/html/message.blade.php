4e6fecb1ecfe8634908190d256b760a8c506882e
bilffy <bilffy.jose@msp.com.au>
2026-05-20 09:45:31 +1000

    template - email testing

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -7,10 +7,8 @@
 </x-mail::header>
 </x-slot:header>
 --}}
-
 {{-- Body --}}
 {{ $slot }}
-
 {{-- Subcopy --}}
 @isset($subcopy)
 <x-slot:subcopy>
@@ -19,19 +17,16 @@
 </x-mail::subcopy>
 </x-slot:subcopy>
 @endisset
-
 {{-- Footer --}}
 <x-slot:footer>
-        <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="750" style="width: 750px; margin: 0 auto;">
-            <tr>
-                <td style="padding: 5px 0 0 0; text-align: center;">
-                    <x-mail::footer>
-						<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3;">
-							Copyright &copy; 2026 MSP Photography. All rights reserved.
-						</p>
-                    </x-mail::footer>
-                </td>
-            </tr>
-        </table>
+<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="width: 100% !important; margin: 0 auto;">
+<tr>
+<td align="center" style="padding: 20px 0 20px 0; text-align: center;">
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3; margin: 0 auto; display: inline-block;">
+Copyright &copy; 2026 MSP Photography. All rights reserved.
+</p>
+</td>
+</tr>
+</table>
 </x-slot:footer>
 </x-mail::layout>
