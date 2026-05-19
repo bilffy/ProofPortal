41fb2c5a3cecd2482c37bbf1029be808b94e95b3
bilffy <bilffy.jose@msp.com.au>
2026-05-19 14:54:21 +1000

    font in email testing

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -1,36 +1,35 @@
 @component('mail::message', ['franchise' => $franchise])
-    <div style="display: none; max-height: 0px; overflow: hidden;">
-        Your MSP Portal Security Code
-    </div>
-    <x-email-wrapper :franchiseName="$franchise->getBusinessName()" :franchisePhone="$franchise->phone" :franchiseEmail="$franchise->email">
-        <tr>
-            <td colspan="2" style="padding: 0px 40px 0px 40px">
-                <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 21px; color: #00b3e0;">
-                    Your MSP Portal Security Code
-                    <br/>
-                </p>
-                <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 14px; color: #666666; line-height: 1.3;">
-                    Hi {{ $user->firstname }},
-                    <br/>
-                    <br/>
-                    To finish logging in to your MSP Portal account, please enter this security code:
-                    <br/>
-                    <br/>
-                </p>
-                <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; letter-spacing: 2px; font-size: 24px; line-height: 1.3; color: #595b5d;">
-                    <strong>{{ $otp }}</strong></p>
-                    
-                    <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 14px; color: #666666; line-height: 1.3;">
-                    <br/>
-                    This code will expire in {{ $expiration }} minutes. 
-                </p>
-                <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; color: #8a8a8a; line-height: 1.3;">
-                    <br/>
-                    <em>Code expired? Click Resend Code on the Security Code screen, or start the login process again. You will receive a new email containing a new security code.</em>
-                    <br/>
-                    <br/>
-                </p>
-            </td>
-        </tr>
-    </x-email-wrapper>
+<div style="display: none; max-height: 0px; overflow: hidden;">
+Your MSP Portal Security Code
+</div>
+<x-email-wrapper :franchiseName="$franchise->getBusinessName()" :franchisePhone="$franchise->phone" :franchiseEmail="$franchise->email">
+<tr>
+<td colspan="2" style="padding: 0px 40px 0px 40px">
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 21px; color: #00b3e0;">
+Your MSP Portal Security Code
+<br/>
+</p>
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 14px; color: #666666; line-height: 1.3;">
+Hi {{ $user->firstname }},
+<br/>
+<br/>
+To finish logging in to your MSP Portal account, please enter this security code:
+<br/>
+<br/>
+</p>
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; letter-spacing: 2px; font-size: 24px; line-height: 1.3; color: #595b5d;">
+<strong>{{ $otp }}</strong></p>
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-size: 14px; color: #666666; line-height: 1.3;">
+<br/>
+This code will expire in {{ $expiration }} minutes. 
+</p>
+<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; color: #8a8a8a; line-height: 1.3;">
+<br/>
+<em>Code expired? Click Resend Code on the Security Code screen, or start the login process again. You will receive a new email containing a new security code.</em>
+<br/>
+<br/>
+</p>
+</td>
+</tr>
+</x-email-wrapper>
 @endcomponent
