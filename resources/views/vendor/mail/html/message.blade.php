2ab610939ff48188ad0a2e5bdc38a038dd0d64ee
bilffy <bilffy.jose@msp.com.au>
2026-05-20 09:48:09 +1000

    template - email testing

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -7,8 +7,10 @@
 </x-mail::header>
 </x-slot:header>
 --}}
+
 {{-- Body --}}
 {{ $slot }}
+
 {{-- Subcopy --}}
 @isset($subcopy)
 <x-slot:subcopy>
@@ -17,16 +19,13 @@
 </x-mail::subcopy>
 </x-slot:subcopy>
 @endisset
+
 {{-- Footer --}}
 <x-slot:footer>
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="width: 100% !important; margin: 0 auto;">
-<tr>
-<td align="center" style="padding: 20px 0 20px 0; text-align: center;">
-<p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3; margin: 0 auto; display: inline-block;">
-Copyright &copy; 2026 MSP Photography. All rights reserved.
-</p>
-</td>
-</tr>
-</table>
+    <x-mail::footer>
+        <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3;">
+            Copyright &copy; 2026 MSP Photography. All rights reserved.
+        </p>
+    </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
