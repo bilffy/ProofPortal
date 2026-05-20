a10d9fde5c17490ff54a940ee11d7fe3baab77d9
bilffy <bilffy.jose@msp.com.au>
2026-05-20 10:27:52 +1000

    template - email testing

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -22,18 +22,13 @@
 
 {{-- Footer --}}
 <x-slot:footer>
-    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="width: 100%; max-width: 750px; margin: 0 auto; border-collapse: collapse;">
-        <tr>
-            <td align="center" style="padding: 25px 0px 25px 0px; text-align: center; vertical-align: top;">
-                <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3;">
-                    <br/>
-                    MSP Photography Pty Ltd
-                    <br/>
-                    Copyright &copy; 2026 MSP Photography. All rights reserved.
-                </p>
-            </td>
-        </tr>
-    </table>
+    <x-mail::footer>
+        <p style="font-family: 'Montserrat', Helvetica, Arial, sans-serif !important; font-size: 12px; color: #666666; line-height: 1.3;">
+            <br/>
+            MSP Photography Pty Ltd
+            <br/>
+            Copyright &copy; 2026 MSP Photography. All rights reserved.
+        </p>
+    </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
-
